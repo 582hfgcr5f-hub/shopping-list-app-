a4f5dbc9cc4d2148b99659152231b33ca6ff89b2
582hfgcr5f-hub <582hfgcr5f@privaterelay.appleid.com>
2026-09-07 23:18:16 -0400

    Force refresh Costco item updates

--- gourmet-glatt-batch5.js
+++ gourmet-glatt-batch5.js
@@ -1,3 +1,3 @@
 document.write('<script src="https://cdn.jsdelivr.net/gh/582hfgcr5f-hub/shopping-list-app-@a68edc6622963242a3092dba865ae0c079a90d1f/gourmet-glatt-batch5.js"><\/script>');
-document.write('<script src="store-tabs.js"><\/script>');
-document.write('<script src="costco-pictures.js"><\/script>');
+document.write('<script src="store-tabs.js?v=20260907-2"><\/script>');
+document.write('<script src="costco-pictures.js?v=20260907-2"><\/script>');
