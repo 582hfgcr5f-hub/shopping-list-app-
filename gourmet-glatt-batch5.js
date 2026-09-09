efadb4d424b12f086b0c402267ee460b75d548f0
582hfgcr5f-hub <582hfgcr5f@privaterelay.appleid.com>
2026-09-09 15:11:23 -0400

    Refresh Costco tabs in Items and Shopping

--- gourmet-glatt-batch5.js
+++ gourmet-glatt-batch5.js
@@ -1,7 +1,7 @@
 document.write('<script src="https://cdn.jsdelivr.net/gh/582hfgcr5f-hub/shopping-list-app-@a68edc6622963242a3092dba865ae0c079a90d1f/gourmet-glatt-batch5.js?v=20260907-3"><\/script>');
-document.write('<script src="store-tabs.js?v=20260909-4"><\/script>');
-document.write('<script src="costco-pictures.js?v=20260909-4"><\/script>');
-document.write('<script src="home-store-shortcuts.js?v=20260909-1"><\/script>');
+document.write('<script src="store-tabs.js?v=20260909-5"><\/script>');
+document.write('<script src="costco-pictures.js?v=20260909-5"><\/script>');
+document.write('<script src="home-store-shortcuts.js?v=20260909-2"><\/script>');
 document.write('<script src="menu-date-storage.js?v=20260909-2"><\/script>');
 document.write('<script src="home-shopping-summary.js?v=20260909-2"><\/script>');
 document.write('<script src="recipes-tab.js?v=20260909-3"><\/script>');
